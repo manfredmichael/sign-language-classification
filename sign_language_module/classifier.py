f34ca7b347f1d5d333c6c17259892985ed6e9780
manfredmichael <koskosanrider@gmail.com>
2022-08-29 18:56:36 +0700

    Add calculate inference time

--- sign_language_module/classifier.py
+++ sign_language_module/classifier.py
@@ -2,26 +2,29 @@ from utils import num2cat, load_image, get_gradcam_visualization
 from models import get_model 
 import torch
 import numpy as np
+import time
 
 model = get_model()
 
 def predict(img, model=model, visualization=False):
+    t0 = time.time()
     predictions = model(img)
+    inference_time = time.time() - t0
     num_class = torch.argmax(predictions).item()
     probs = torch.nn.functional.softmax(predictions, dim=1)
     confidence_score = "{:.3f}".format(probs[0][num_class].item())
     result = num2cat[num_class]
     if visualization:
         visualization = get_gradcam_visualization(model, img, result)
-        return result, confidence_score, visualization
-    return result, confidence_score
+        return result, confidence_score, inference_time, visualization
+    return result, confidence_score, inference_time
 
 
 if __name__ == '__main__':
     img = load_image("img/B.png")
     a = torch.squeeze(img, axis=0).numpy()
     
-    result, confidence_score, visualization = predict(img, model=model, visualization=True)
+    result, confidence_score, inference_time, visualization = predict(img, model=model, visualization=True)
 
     import matplotlib.pyplot as plt
     plt.figure(figsize=(10, 10))
