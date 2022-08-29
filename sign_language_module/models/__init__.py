7791d13f3640d332e25622b4ebd6a905971836c5
manfredmichael <koskosanrider@gmail.com>
2022-08-29 18:56:56 +0700

    Modify model checkpoint path

--- sign_language_module/models/__init__.py
+++ sign_language_module/models/__init__.py
@@ -3,7 +3,7 @@ from models.ASLModel import ASLModel
 from utils import num2cat
 
 def get_model():
-    weights = torch.load("checkpoints/ASL_model1.pt")
+    weights = torch.load("checkpoints/ASL_model.pt")
     model = ASLModel(num_classes=len(num2cat))
     model.load_state_dict(weights)
     return model
